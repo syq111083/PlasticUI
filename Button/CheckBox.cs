fc8d2eef97bd8fd48a8a3a81303cc6239a2be2b8
shiyunqing <syq111083@hotmail.com>
2023-02-03 13:51:32 +0800

    add checkbox

--- Button/CheckBox.cs
+++ Button/CheckBox.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,35 +16,12 @@ using System.Windows.Shapes;
 
 namespace Plastic
 {
-    /// <summary>
-    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
-    ///
-    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
-    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
-    /// 元素中:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Plastic"
-    ///
-    ///
-    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
-    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
-    /// 元素中:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:Plastic;assembly=Plastic"
-    ///
-    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
-    /// 并重新生成以避免编译错误:
-    ///
-    ///     在解决方案资源管理器中右击目标项目，然后依次单击
-    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
-    ///
-    ///
-    /// 步骤 2)
-    /// 继续操作并在 XAML 文件中使用控件。
-    ///
-    ///     <MyNamespace:CheckBox/>
-    ///
-    /// </summary>
+    [TemplatePart(Name = "rect0", Type = typeof(Rectangle))]
+    [TemplatePart(Name = "rect1", Type = typeof(Rectangle))]
+    [TemplateVisualState(GroupName = "SelectStates", Name = "Unselected")]
+    [TemplateVisualState(GroupName = "SelectStates", Name = "Selected")]
+    [TemplateVisualState(GroupName = "FocusStates", Name = "Focused")]
+    [TemplateVisualState(GroupName = "FocusStates", Name = "Unfocused")]
     public class CheckBox : System.Windows.Controls.CheckBox
     {
         static CheckBox()
@@ -51,7 +29,82 @@ namespace Plastic
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CheckBox), new FrameworkPropertyMetadata(typeof(CheckBox)));
         }
 
-        public static readonly DependencyProperty BorderWidthProperty = DependencyProperty.Register("BorderWidth", typeof(double), typeof(CheckBox), new PropertyMetadata((double)24, new PropertyChangedCallback(SetBorderWidth)));
+        private Rectangle? rect0;
+
+        public Rectangle? Rect0
+        {
+            get { return rect0; }
+            set
+            {
+                if (rect0 != null)
+                    rect0.MouseLeftButtonDown -= new MouseButtonEventHandler(LeftButtonDown);
+                rect0 = value;
+                if (rect0 != null)
+                    rect0.MouseLeftButtonDown += new MouseButtonEventHandler(LeftButtonDown);
+            }
+        }
+
+        private Border? border0;
+
+        public Border? Border0
+        {
+            get { return border0; }
+            set
+            {
+                if (border0 != null)
+                {
+                    border0.FocusableChanged -= Border0_FocusableChanged;
+                    border0.MouseLeftButtonDown -= new MouseButtonEventHandler(LeftButtonDown);
+                }
+                border0 = value;
+                if (border0 != null)
+                {
+                    border0.FocusableChanged += Border0_FocusableChanged;
+                    border0.MouseLeftButtonDown += new MouseButtonEventHandler(LeftButtonDown);
+                }
+            }
+        }
+
+        private void Border0_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            base.OnGotFocus(null);
+            if (Border0?.IsFocused == true)
+            {
+                VisualStateManager.GoToState(this, "Focused", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Unfocused", true);
+
+            }
+        }
+
+        void LeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnClick();
+            if (IsChecked == true)
+            {
+                IsChecked = false;
+                VisualStateManager.GoToState(this, "Unselected", true);
+            }
+            else
+            {
+                IsChecked = true;
+                VisualStateManager.GoToState(this, "Selected", true);
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnClick();
+            VisualStateManager.GoToState(this, "Unselected", true);
+            Rect0 = GetTemplateChild("rect0") as Rectangle;
+            Border0 = GetTemplateChild("border") as Border;
+        }
+
+
+
+        public static readonly DependencyProperty BorderWidthProperty = DependencyProperty.Register("BorderWidth", typeof(double), typeof(CheckBox), new PropertyMetadata((double)24));
 
         public double BorderWidth
         {
@@ -59,28 +112,31 @@ namespace Plastic
             set { SetValue(BorderWidthProperty, value); }
         }
 
-        static void SetBorderWidth(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            CheckBox cb = (CheckBox)d;
-            double value = (double)e.NewValue;
-            cb.BorderWidth = value;
-            cb.Width = value - 8;
-        }
 
-        static void SetBorderHeight(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            CheckBox cb = (CheckBox)d;
-            double value = (double)e.NewValue;
-            cb.BorderHeight = value;
-            cb.Height = value - 8;
-        }
-
-        public static readonly DependencyProperty BorderHeightProperty = DependencyProperty.Register("BorderHeight", typeof(double), typeof(CheckBox), new PropertyMetadata((double)24, new PropertyChangedCallback(SetBorderHeight)));
+        public static readonly DependencyProperty BorderHeightProperty = DependencyProperty.Register("BorderHeight", typeof(double), typeof(CheckBox), new PropertyMetadata((double)24));
 
         public double BorderHeight
         {
             get { return (double)GetValue(BorderHeightProperty); }
             set { SetValue(BorderHeightProperty, value); }
+        }
+
+        public new double Width
+        {
+            get { return (double)GetValue(WidthProperty); }
+            set
+            {
+                SetValue(WidthProperty, value);
+            }
+        }
+
+        public new double Height
+        {
+            get { return (double)GetValue(HeightProperty); }
+            set
+            {
+                SetValue(HeightProperty, value);
+            }
         }
     }
 }
