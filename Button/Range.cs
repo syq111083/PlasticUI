7685c7ecace7ff25f79a09ac76a8bf35fea6b35b
shiyunqing <syq111083@hotmail.com>
2023-02-14 17:25:50 +0800

    add Range

---
+++ Button/Range.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plastic
+{
+    public class Range : Slider
+    {
+        static Range()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Range), new FrameworkPropertyMetadata(typeof(Range)));
+        }
+
+        private Thumb thumb0;
+
+        public Thumb Thumb0
+        {
+            get { return thumb0; }
+            set { thumb0 = value; }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Thumb0 = GetTemplateChild("thumb0") as Thumb;
+        }
+
+        protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseDown(e);
+            Thumb0.Focus();
+        }
+
+    }
+}
---
+++ Button/Thumb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plastic
+{
+
+    public class Thumb : System.Windows.Controls.Primitives.Thumb
+    {
+        static Thumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Thumb), new FrameworkPropertyMetadata(typeof(Thumb)));
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            VisualStateManager.GoToState(this, "Focus", true);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            VisualStateManager.GoToState(this, "Unfocus", true);
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            VisualStateManager.GoToState(this, "Pressed", true);
+        }
+
+        
+    }
+}
