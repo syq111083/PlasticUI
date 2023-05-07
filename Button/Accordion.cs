bf17f3839b5735eaeb117af725da7a1e2e9fbb04
施云卿 <syq111083@hotmail.com>
2023-05-07 12:51:01 +0800

    accordion add animation

---
+++ Button/Accordion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plastic
+{
+    public class Accordion : Expander
+    {
+        static Accordion()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Accordion), new FrameworkPropertyMetadata(typeof(Accordion)));
+        }
+
+        protected override void OnExpanded()
+        {
+            base.OnExpanded();
+            VisualStateManager.GoToState(this, "Expand", true);
+            Focus();
+        }
+
+        protected override void OnCollapsed()
+        {
+            base.OnCollapsed();
+            VisualStateManager.GoToState(this, "Collapse", true);
+
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            VisualStateManager.GoToState(this, "BorderChange", true);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            VisualStateManager.GoToState(this, "BorderResume", true);
+
+        }
+    }
+}
