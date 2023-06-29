d40888e7e3f257973660a70dcc34f78478d73ed4
施云卿 <syq111083@hotmail.com>
2023-06-30 00:09:45 +0800

    Button brightness property support LinearGradientBrush now;

--- Plastic.Framework/Button.cs
+++ Plastic.Framework/Button.cs
@@ -62,27 +62,67 @@ namespace Plastic.Framework
 
         public Color? OriginBrush;
 
+        public LinearGradientBrush OriginGradient = null;
+
         private static void OnBrightnessChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             Button btn = (Button)d;
-            SolidColorBrush background = (SolidColorBrush)btn.Background;
-            Color backcolor = background.Color;
-            if (btn.OriginBrush == null)
+            Type type = btn.Background.GetType();
+            if (type == typeof(SolidColorBrush))
             {
-                btn.OriginBrush = backcolor;
+                SolidColorBrush background = (SolidColorBrush)btn.Background;
+                Color backcolor = background.Color;
+                if (btn.OriginBrush == null)
+                {
+                    btn.OriginBrush = backcolor;
+                }
+                byte r = (byte)(btn.OriginBrush?.R * btn.Brightness);
+                if (r < btn.OriginBrush?.R && btn.Brightness > 1)
+                    r = 255;
+                byte g = (byte)(btn.OriginBrush?.G * btn.Brightness);
+                if (g < btn.OriginBrush?.G && btn.Brightness > 1)
+                    g = 255;
+                byte b = (byte)(btn.OriginBrush?.B * btn.Brightness);
+                if (b < btn.OriginBrush?.B && btn.Brightness > 1)
+                    b = 255;
+                Color newColor = Color.FromArgb(255, r, g, b);
+                btn.Background = new SolidColorBrush(newColor);
             }
-
-            byte r = (byte)(btn.OriginBrush?.R * btn.Brightness);
-            if (r<btn.OriginBrush?.R && btn.Brightness>1)
-                r = 255;
-            byte g = (byte)(btn.OriginBrush?.G * btn.Brightness);
-            if (g<btn.OriginBrush?.G && btn.Brightness>1)
-                g = 255;
-            byte b = (byte)(btn.OriginBrush?.B * btn.Brightness);
-            if (b<btn.OriginBrush?.B &&btn.Brightness>1)
-                b = 255;
-            Color newColor = Color.FromArgb(255, r, g, b);
-            btn.Background = new SolidColorBrush(newColor);
+            else if (type == typeof(LinearGradientBrush))
+            {
+                LinearGradientBrush linear = (LinearGradientBrush)btn.Background;
+                if (btn.OriginGradient == null)
+                {
+                    btn.OriginGradient = linear.CloneCurrentValue();
+                }
+                var stops = btn.OriginGradient.GradientStops;
+                List<GradientStop> gradientStops = new List<GradientStop>();
+                for (int i = 0; i < linear.GradientStops.Count; i++)
+                {
+                    Color backcolor =stops[i].Color;
+                    byte r = (byte)(stops[i].Color.R * btn.Brightness);
+                    if (r < backcolor.R && btn.Brightness > 1)
+                        backcolor.R = 255;
+                    else
+                        backcolor.R = r;
+                    byte g = (byte)(stops[i].Color.G * btn.Brightness);
+                    if (g < backcolor.G && btn.Brightness > 1)
+                        backcolor.G = 255;
+                    else
+                        backcolor.G = g;
+                    byte b = (byte)(stops[i].Color.B * btn.Brightness);
+                    if (b < backcolor.B && btn.Brightness > 1)
+                        backcolor.B = 255;
+                    else backcolor.B = b;
+                    GradientStop stop = new GradientStop(){Color = backcolor,Offset=stops[i].Offset};
+                    gradientStops.Add(stop);
+                }
+                linear.GradientStops.Clear();
+                foreach (var item in gradientStops)
+                {
+                    linear.GradientStops.Add(item);
+                }
+            }
         }
 
         public static readonly DependencyProperty SizeProperty = DependencyProperty.Register("Size", typeof(SizeEnum), typeof(Button), new PropertyMetadata(SizeEnum.Default));
